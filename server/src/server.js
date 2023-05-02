9537813e3273684fd9af7acf610457d13ba64519
aavetisyanit <aavetisyanit@gmail.com>
2023-05-02 15:54:55 -0500

    Define get planets router and controller

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(() => {
+server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
