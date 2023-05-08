90cfc19ba54b19b809bf3877e5fd7c5b714bbbd8
aavetisyanit <aavetisyanit@gmail.com>
2023-05-08 15:26:17 -0500

    Add request property validation in add launches controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,18 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
 	const launch = req.body;
+	if (
+		!launch.mission ||
+		!launch.rocket ||
+		!launch.destination ||
+		!launch.launchDate
+	) {
+		return res.status(200).json({ error: 'Missing required launch property' });
+	}
 	launch.launchDate = new Date(launch.launchDate);
+	if (isNaN(launch.launchDate)) {
+		return res.status(200).json({ error: 'Invalid lunch date' });
+	}
 
 	addNewLaunch(launch);
 	return res.status(201).json(launch);
