fe65b906812f959c168de9f7e045382c36e905f3
aavetisyanit <aavetisyanit@gmail.com>
2023-05-15 16:51:19 -0500

    Connect to mongoDB with mongoose dependency

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,25 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
 const { loadPlanetData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+	'mongodb+srv://nasa-api:xPFfRY4acW8x4Uet@cluster0.iujsfi3.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+	console.log('MongoDB connection is ready');
+});
+
+mongoose.connection.on('error', error => {
+	console.error(error);
+});
+
 async function startServer() {
+	await mongoose.connect(MONGO_URL);
 	await loadPlanetData();
 
 	server.listen(PORT, () => {
