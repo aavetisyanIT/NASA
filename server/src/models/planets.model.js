9edba49fe26488147df1a46ee18b5e10a0eda409
aavetisyanit <aavetisyanit@gmail.com>
2023-05-16 19:06:05 -0500

    Update loadPlanetData and getAllPlanets to use mongoDB

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 const path = require('path');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
 	return (
@@ -24,23 +24,43 @@ const loadPlanetData = () => {
 					columns: true,
 				}),
 			)
-			.on('data', data => {
+			.on('data', async data => {
 				if (isHabitablePlanet(data)) {
-					habitablePlanets.push(data);
+					savePlanet(data);
 				}
 			})
 			.on('error', err => {
 				console.log(err);
 				reject(err);
 			})
-			.on('end', () => {
+			.on('end', async () => {
+				const planetsNum = (await getAllPlanets()).length;
+				console.log(`We have ${planetsNum} planets`);
 				resolve();
 			});
 	});
 };
 
-function getAllPlanets() {
-	return habitablePlanets;
+async function getAllPlanets() {
+	return await planets.find({});
+}
+
+async function savePlanet(planet) {
+	try {
+		await planets.updateOne(
+			{
+				keplerName: planet.kepler_name,
+			},
+			{
+				keplerName: planet.kepler_name,
+			},
+			{
+				upsert: true,
+			},
+		);
+	} catch (error) {
+		console.log(`Planets are not saved. Error: ${error}`);
+	}
 }
 
 module.exports = {
