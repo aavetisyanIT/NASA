598fa6f38514b0759e82a732d7567843d57514f0
aavetisyanit <aavetisyanit@gmail.com>
2023-05-18 14:37:42 -0500

    Update unit tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const app = require('../../app');
+const { mongoConnect } = require('../../services/mongo');
 
 describe('Test GET /launches', () => {
+	beforeAll(async () => {
+		await mongoConnect();
+	});
 	test('Is should respond with 200', async () => {
 		await request(app)
 			.get('/launches')
